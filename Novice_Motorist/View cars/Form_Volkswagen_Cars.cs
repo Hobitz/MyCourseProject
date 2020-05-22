363f081a9bca65a5c3104b72edd1e6184f301bee
romalyshik@gmail.com <romalyshik@gmail.com>
2020-05-22 14:35:20 +0300

    Added Form Brand_Cars

---
+++ Novice_Motorist/View cars/Form_Volkswagen_Cars.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Novice_Motorist.View_cars
+{
+    public partial class Form_Volkswagen_Cars : Form
+    {
+        public Form_Volkswagen_Cars()
+        {
+            InitializeComponent();
+        }
+    }
+}
